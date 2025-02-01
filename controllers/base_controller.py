20fde4b3c106a075944381da442015e3b9071999
OmarAllam22 <omar22allam@gmail.com>
2025-02-01 14:20:29 +0200

    organizing repository

--- controllers/base_controller.py
+++
@@ -1,9 +0,0 @@
-# adding the parent directory to the sys path to enble relative import
-
-from helpers.validate_and_load_dotenv_vars import AppConfig
-
-class BaseController:
-    def __init__(self):
-        self.app_settings = AppConfig() # contains all the .env variables in addition to get_settings() method
-        
-
